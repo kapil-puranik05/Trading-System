f38ea0b48a4b16b9c3f0cdfb1fad9b73f8afc4e9
Kapil Puranik <kapilpuranik764@gmail.com>
2025-11-25 21:16:18 +0530

    Added Controller layer and removed security

--- services/users/src/main/java/com/trade/users/controllers/UserController.java
+++ services/users/src/main/java/com/trade/users/controllers/UserController.java
@@ -1,10 +1,34 @@
 package com.trade.users.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.trade.users.dtos.UserRequestDTO;
+import com.trade.users.models.AppUser;
+import com.trade.users.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
+    @PostMapping
+    public ResponseEntity<AppUser> signup(@RequestBody UserRequestDTO userRequestDTO) {
+        return new ResponseEntity<>(userService.registerUser(userRequestDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<AppUser> getUser(@PathVariable UUID userId) {
+        return new ResponseEntity<>(userService.getUser(userId), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<AppUser>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
 }
